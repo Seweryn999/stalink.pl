95620661ef2a471a871f042210b014f9c2a99a8e
Seweryn <sewcio0212@gmail.com>
2025-04-03 22:42:44 +0200

    next config adjustment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/stalink-pl",
-  assetPrefix: "/stalink-pl/",
-  output: "export",
+  basePath: "/stalink-pl", // <- Bardzo ważne, musi pasować do nazwy repozytorium
+  assetPrefix: "/stalink-pl/", // <- Bardzo ważne, musi pasować do nazwy repozytorium
+  output: "export", // <- To musi być dodane, aby utworzyć folder `out`
 };
 
 module.exports = nextConfig;
