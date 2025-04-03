63e6adb53eb2a59329052027fb48ce148b4dfff5
Seweryn <sewcio0212@gmail.com>
2025-04-03 21:50:56 +0200

    error fix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   basePath: "/stalink-pl",
   assetPrefix: "/stalink-pl/",
+  output: "export", // <- DODAJ TO
 };
 
 module.exports = nextConfig;
