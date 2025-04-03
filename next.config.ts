59f9bf0f9881e1bd9ec8e0062cbaec325572d6eb
Seweryn <sewcio0212@gmail.com>
2025-04-03 22:00:39 +0200

    correct repo name

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   basePath: "/stalink-pl",
   assetPrefix: "/stalink-pl/",
-  output: "export", // <- DODAJ TO
+  output: "export",
 };
 
 module.exports = nextConfig;
