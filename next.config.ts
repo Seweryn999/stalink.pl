c5172c0a901152c086bf93dcf3af4ffe630ed6f4
Seweryn <sewcio0212@gmail.com>
2025-04-04 11:32:52 +0200

    vercel config (test.2)

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "", // <- Bardzo ważne, musi pasować do nazwy repozytorium
-  assetPrefix: "", // <- Bardzo ważne, musi pasować do nazwy repozytorium
-  // output: "export",
+  // basePath: "/stalink-pl", // <- Bardzo ważne, musi pasować do nazwy repozytorium
+  // assetPrefix: "/stalink-pl/", // <- Bardzo ważne, musi pasować do nazwy repozytorium
+  // output: "export", // <- To musi być dodane, aby utworzyć folder `out`
 };
 
 module.exports = nextConfig;
