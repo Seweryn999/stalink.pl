34d08435febc38e6f29caf15d42a896d03bc31a3
Seweryn <sewcio0212@gmail.com>
2025-04-05 13:09:33 +0200

    vercel analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { ReactNode } from "react";
 import StarsBackground from "../components/ui/StarsBackground";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Stalink",
@@ -14,6 +15,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className="relative text-white overflow-x-hidden min-h-screen starry-background">
+        <StarsBackground />
+        <Analytics />
         <StarsBackground />
         <div className="relative z-10">{children}</div>
       </body>
